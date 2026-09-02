044d4e8d4a054236f185d308e6e51fdca3b6c325
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-09-02 11:00:13 +0530

    Final commit Assignment 4

--- Assignment_1.3/submissions/PranabeshBasu_12023002028154_NilanjanSaha_12023002028144_PritamKarak_12023002028157_PurbaChakraborty_12023002028162/Assignment_4.py
+++ Assignment_1.3/submissions/PranabeshBasu_12023002028154_NilanjanSaha_12023002028144_PritamKarak_12023002028157_PurbaChakraborty_12023002028162/Assignment_4.py
@@ -32,21 +32,21 @@ driver.maximize_window()
 
 # Approach 1: CSS Child Selector
 # Locate a label that is a direct child of fieldset
-# element = driver.find_element(
-#     By.CSS_SELECTOR,
-#     "fieldset > label > input#checkBoxOption2"
-# )
+element = driver.find_element(
+    By.CSS_SELECTOR,
+    "fieldset > label > input#checkBoxOption2"
+)
 
-# element.click()
+element.click()
 
 # Approach 2: CSS Adjacent Sibling Selector
 # Locate the label immediately following checkbox Option 2
-option2 = driver.find_element(
-    By.CSS_SELECTOR,
-    "label[for='radio1'] + label[for='radio2'] > input[value='radio2']"
-)
+# option2 = driver.find_element(
+#     By.CSS_SELECTOR,
+#     "label[for='radio1'] + label[for='radio2'] > input[value='radio2']"
+# )
 
-option2.click()
+# option2.click()
 
 
 time.sleep(10)
